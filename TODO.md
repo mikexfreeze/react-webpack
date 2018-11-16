22402fdb5e05063a57f9ba14c455fab028fe8e0e
Unknown <mikexfreeze@gmail.com>
2018-11-16 18:05:24 +0800

    react babel-loader

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO List
+
+- [x] TreeShaking
+- index template
---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+/* create by Micheal Xiao 2018/11/7 16:52 */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ],
+
+};
